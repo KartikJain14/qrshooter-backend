ea1dcca5ee11e5d8e5f7c04f03955faf5234ec2d
karti <kartikvyomfortp@gmail.com>
2025-01-25 20:57:28 +0530

    Add find by phone route

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.auth_controller import send_verification_code, verify_code, create_user_by_token, search_user_by_email
+from controllers.auth_controller import send_verification_code, verify_code, create_user_by_token, search_user_by_email, search_user_by_phone
 
 auth_routes = Blueprint('auth_routes', __name__)
 
@@ -18,3 +18,7 @@ def add_user_route():
 @auth_routes.route('/user/by_email', methods=['POST'])
 def search_user_by_email_route():
     return search_user_by_email()
+
+@auth_routes.route('/user/by_phone', methods=['POST'])
+def search_user_by_phone_route():
+    return search_user_by_phone()
