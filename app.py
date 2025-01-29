b8bfb8c69cc47c2b8d5b5abe261f867924ae8275
karti <kartikvyomfortp@gmail.com>
2025-01-29 09:05:47 +0530

    Register blueprints for the app

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, Blueprint
 from config import Config
-from routes.admin_routes import admin_routes
-from routes.credit_routes import credit_routes
-from routes.user_routes import user_routes
-from routes.auth_routes import auth_routes
-from routes.website_routes import website_routes
-
+from routes import (
+    admin_routes,
+    credit_routes,
+    user_routes,
+    auth_routes,
+    website_routes,
+    data_routes
+    )
 import os
 
 ADMIN_PATH=os.getenv("ADMIN_PORTAL") or "/admin"
@@ -23,6 +25,7 @@ app.register_blueprint(admin_routes, url_prefix='/admin')
 app.register_blueprint(credit_routes)
 app.register_blueprint(user_routes, url_prefix="/user")
 app.register_blueprint(auth_routes)
+app.register_blueprint(data_routes)
 
 if __name__ == '__main__':
     app.run(debug=True, port=app.config['PORT'])
